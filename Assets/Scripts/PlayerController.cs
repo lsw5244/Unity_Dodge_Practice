d7169c1b206cd285776cd267eb2f82edcd8b5d6f
이성우 <lsw5244@naver.com>
2022-01-07 02:11:03 +0900

    feat : PlayerController구현

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private Rigidbody rigi;
+
+    public float speed = 5.0f;
+
+    void Start()
+    {
+        rigi = GetComponent<Rigidbody>();       
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float velX = Input.GetAxis("Horizontal");
+        float velZ = Input.GetAxis("Vertical");
+
+        rigi.velocity = new Vector3(velX * speed, 0, velZ * speed);\
+    }
+
+    public void Die()
+    {
+        gameObject.SetActive(false);
+    }
+}
